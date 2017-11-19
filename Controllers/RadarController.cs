e057842ca3d3642bbed136d4a0b203a7860db924
x00n <burgio@gmail.com>
2017-11-19 11:47:24 +0100

    First commit of Radar App

---
+++ Controllers/RadarController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace radar.Controllers
+{
+    [Route("api/[controller]")]
+    public class RadarController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<EnemyCoordinates> Get()
+        {
+            return new EnemyCoordinates[] { new EnemyCoordinates{ X = 0.2424, Y = 0.32412 } };
+        }
+    }
+}
