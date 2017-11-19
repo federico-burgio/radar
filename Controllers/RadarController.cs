841d4ef7ade07acbee5ba101b5526c0a50e7b21b
x00n <burgio@gmail.com>
2017-11-19 21:14:57 +0100

    Added random behaviour

--- Controllers/RadarController.cs
+++ Controllers/RadarController.cs
@@ -9,11 +9,29 @@ namespace radar.Controllers
     [Route("api/[controller]")]
     public class RadarController : Controller
     {
-        // GET api/values
+        // Check the space in front of the radar for enemies and return the list of enemies as coordinates
         [HttpGet]
         public IEnumerable<EnemyCoordinates> Get()
         {
-            return new EnemyCoordinates[] { new EnemyCoordinates{ X = 0.2424, Y = 0.32412 } };
+            List<EnemyCoordinates> foundEnemies = new List<EnemyCoordinates>();
+            Random random = new Random();
+            int numOfEnemies = random.Next(0,5);
+
+            for(int i=0; i< numOfEnemies; i++)
+            {
+                foundEnemies.Add(new EnemyCoordinates()
+                { 
+                    X = GetRandomCoordinate(random, -1,1), 
+                    Y = GetRandomCoordinate(random, -1,1)
+                });
+            }
+
+            return foundEnemies;
+        }
+
+        public double GetRandomCoordinate(Random random, double minimum, double maximum)
+        { 
+            return random.NextDouble() * (maximum - minimum) + minimum;
         }
     }
 }
